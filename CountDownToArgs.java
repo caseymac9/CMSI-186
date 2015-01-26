a4e34b55887b4829e6eccda4e6efb6fdf60ed0aa
caseymac9 <caseymac9@users.noreply.github.com>
2015-01-26 15:55:27 -0800

    Create CountDownToArgs.java

---
+++ CountDownToArgs.java
@@ -0,0 +1,17 @@
+public class CountDownToArgs{
+	public static void main(String args[]){
+		try{
+			int startNum = Integer.parseInt(args[0]);
+			if(startNum < 0){
+			    System.out.println("Cannot accept negative numbers.");
+			}
+	        for(int i = startNum; i >= 0; i--){
+	            System.out.println(i + " ");
+	        }
+		} catch (ArrayIndexOutOfBoundsException e){
+		    System.out.println("No Arguments Provided.");
+		} catch (NumberFormatException e2) {
+	        System.out.println("Please only insert integers.");
+	  }
+  }
+}
